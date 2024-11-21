85320a173513df0e99672e493564cd9e85be10be
Ervin Natareno <ean1207@gmail.com>
2024-11-21 14:15:29 -0800

    Added delete function to delete exercises you want to remove when creating your workouts

--- client/src/components/CreateWorkout.jsx
+++ client/src/components/CreateWorkout.jsx
@@ -8,7 +8,7 @@ export default function CreateWorkout() {
 
     const [workoutTitle, setWorkoutTitle] = useState(''); // State to store the title of the workout
     const [exercises, setExercises] = useState([ // State to hold a list of exercises; each exercise includes name, sets, reps, and weight
-        { name: '', sets: 0, reps: 0, weight: 0 },
+        { name: '', sets: 0},
     ]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -37,22 +37,29 @@ export default function CreateWorkout() {
         });
     }, [navigate]);
 
-    // Function to add a new exercise row with default values (empty name, 0 sets, reps, and weight)
+    // Function to add a new exercise row with default values (empty name, 0 sets)
     const handleAddExercise = () => {
-        setExercises([...exercises, { name: '', sets: 0, reps: 0, weight: 0 }])
+        setExercises([...exercises, { name: '', sets: 0}])
     }
 
-    // Function to update the value of a specific field (e.g., name, sets, reps, weight)
+    // Function to update the value of a specific field (e.g., name, sets)
     // for a specific exercise identified by its index
     const handleExerciseChange = (index, field, value) => {
         const newExercise = [...exercises]; // Create a copy of the exercises array
         newExercise[index][field] = value; // Update the specific field of the selected exercise
         setExercises(newExercise) // Update the exercises state with the modified array
+
     };
 
     // Function to handle changes to the workout title input field
     const handleWorkoutTitleChange = (e) => {
         setWorkoutTitle(e.target.value) // Update the state with the new workout title
+    }
+
+    const handleDeleteExercise = (index) => {
+        const newExercises = [...exercises]
+        newExercises.splice(index, 1)
+        setExercises(newExercises)
     }
 
     const handleSubmit = () => {
@@ -83,39 +90,35 @@ export default function CreateWorkout() {
             />
 
             {exercises.map((exercise, index) => (
-                <div key={index} className="space-y-2">
-                <input
-                    type="text"
-                    value={exercise.name}
-                    onChange={(e) =>
-                    handleExerciseChange(index, "name", e.target.value)
-                    }
-                    placeholder="Exercise Name"
-                    className="border border-gray-400 px-2 py-1 rounded"
-                />
-                <input
-                    type="number"
-                    value={exercise.sets}
-                    onChange={(e) => handleExerciseChange(index, "sets", e.target.value)}
-                    placeholder="Sets"
-                    className="border border-gray-400 px-2 py-1 rounded"
-                />
-                <input
-                    type="number"
-                    value={exercise.reps}
-                    onChange={(e) => handleExerciseChange(index, "reps", e.target.value)}
-                    placeholder="Reps"
-                    className="border border-gray-400 px-2 py-1 rounded"
-                />
-                <input
-                    type="number"
-                    value={exercise.weight}
-                    onChange={(e) =>
-                    handleExerciseChange(index, "weight", e.target.value)
-                    }
-                    placeholder="Weight"
-                    className="border border-gray-400 px-2 py-1 rounded"
-                />
+                <div key={index} className="flex flex-row items-center gap-5">
+                    
+                        <div className='flex flex-col'>
+                            <div className="text-white">Exercise Name</div>
+                            <input
+                            type="text"
+                            value={exercise.name}
+                            onChange={(e) =>
+                            handleExerciseChange(index, "name", e.target.value)
+                            }
+                            className="border border-gray-400 px-2 py-1 rounded"
+                            />
+                        </div>
+                        <div className='flex flex-col'>
+                            <div className="text-white">Sets</div>
+                            <input
+                                type="number"
+                                value={exercise.sets}
+                                onChange={(e) => handleExerciseChange(index, "sets", e.target.value)}
+                                className="border border-gray-400 px-2 py-1 rounded"
+                            />
+                        </div>
+                        {/* Delete Workout */}
+                        <button onClick={() => handleDeleteExercise(index)} className='text-red-500 mt-5'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+
+                        </button>
                 </div>
             ))}
 
--- server/models/Workouts.js
+++ server/models/Workouts.js
@@ -2,20 +2,47 @@ const mongoose = require("mongoose");
 
 // Define a schema for individual exercises
 const ExerciseSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    sets: { type: Number, required: true },
-    reps: { type: Number, required: true },
-    weigth: { type: Number, required: true },
-    personalRecord: { type: Boolean, default: false }
+    name: { 
+        type: String, 
+        required: true 
+    },
+    sets: { 
+        type: Number, 
+        required: true 
+    },
+    reps: { 
+        type: Number, 
+        required: trfalseue 
+    },
+    weight: { 
+        type: Number, 
+        required: false 
+    },
+    personalRecord: { 
+        type: Boolean, 
+        default: false 
+    }
 });
 
 // Define a schema for a workout that includes multiple exercises
 const WorkoutSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { 
+        type: String, 
+        required: true 
+    },
     exercise: [ExerciseSchema], // Array of exercises
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    createAt: { type: Date, default: Date.now },
-    completed: { type: Boolean, default: false }
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User" 
+    },
+    createAt: { 
+        type: Date, 
+        default: Date.now 
+    },
+    completed: { 
+        type: Boolean, 
+        default: false 
+    }
 });
 
 module.exports = mongoose.model("Workout", WorkoutSchema);
